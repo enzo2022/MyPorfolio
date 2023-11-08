74560a2f71601c5020eedc801479de355ca0d618
enzo2022 <you@example.com>
2023-11-08 09:58:03 -0300

    diseño

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -8,12 +8,12 @@ export default function Footer(){
     return(
 
         <div className='footer'>
-            <p>Mis redes sociales, a través de las cuales puedes contactarme:
+            <p>Mis redes sociales, a través de las cuales puedes contactarme:</p>
          <ul className='redes'>
          <a href="https://www.linkedin.com/in/enzo-monti-65bb9a19a/"><img src={linkedin} alt="1" /></a>
             <a href="#https://www.facebook.com/enzo.monti.7"><img src={facebook} alt="2" /></a>
             <a href="#https://www.instagram.com/enzomonti_ok/"><img src={instagram} alt="3" /></a>
-       </ul>  </p>
+       </ul>  
 
 </div> 
 
